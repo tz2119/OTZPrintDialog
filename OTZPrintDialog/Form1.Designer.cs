b751222b8322830bfabc9b2fbf6ffa8f0dd78443
TZ <88926862+tz2119@users.noreply.github.com>
2022-02-26 20:08:39 +0900

    Update Form1.Designer.cs

--- OTZPrintDialog/Form1.Designer.cs
+++ OTZPrintDialog/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 46);
             this.button1.TabIndex = 0;
-            this.button1.Text = "印刷ダイアログ表示";
+            this.button1.Text = "印刷ダイアログの表示";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
